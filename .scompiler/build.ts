e9ff9fad230915c5f61bd2403d65795dadba44b1
Kos <kostyaveselovsky@gmail.com>
2021-08-09 15:22:02 +0300

    fix: build pages with SVG icons

--- .scompiler/build.ts
+++ .scompiler/build.ts
@@ -6,13 +6,26 @@ import { PagesModule } from "./modules/pages";
 import { JsModule } from "./modules/js";
 import { Filesystem } from "./fs";
 import { SvgModule } from "./modules/svg";
+import { ReactNode } from "react";
 
 export async function build(config: Config, memFs: Filesystem) {
+    function makeSvgFn(iconResolver: (icon: string) => ReactNode) {
+        return (icon: string) => {
+            const result = iconResolver(icon);
+
+            if (result) {
+                return result;
+            }
+
+            return `Icon ${icon} not found!`;
+        };
+    }
+
     const pagesModule = new PagesModule(config, memFs, () => ({
         pageId: 0,
         pageUrl: '',
         resizeFn: imagesModule.makeResizeFn(true),
-        svgFn: () => null,
+        svgFn: makeSvgFn(config.iconResolver || (() => null)),
     }));
     const imagesModule = new ImagesModule(config, memFs);
     const copyModule = new CopyModule(config, memFs);
